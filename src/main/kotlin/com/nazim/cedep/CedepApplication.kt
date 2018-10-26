57e9cb62fe5d3414e9038e53c3f5f647688f7fc9
thejeziera <jezierskimichal1@gmail.com>
2018-10-26 14:48:35 +0200

    Add files via upload

---
+++ src/main/kotlin/com/nazim/cedep/CedepApplication.kt
@@ -0,0 +1,11 @@
+package com.nazim.cedep
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CedepApplication
+
+fun main(args: Array<String>) {
+    runApplication<CedepApplication>(*args)
+}
---
+++ src/test/kotlin/com/nazim/cedep/CedepApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.nazim.cedep
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class CedepApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
